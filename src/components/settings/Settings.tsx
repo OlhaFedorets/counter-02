432e988b8652b73885c09e178a62d106433e5779
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-25 18:10:52 +0300

    change settings

---
+++ src/components/settings/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Settings = () => {
+    return (
+        <form>
+            <label htmlFor={'maxValue'}>max value:</label>
+            <input id={'maxValue'} type={''}/>
+            start value:
+            <input/>
+        </form>
+    );
+};
