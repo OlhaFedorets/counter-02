dd2797efba24c53c9ceb1c099f64613a6a956a09
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-26 16:40:11 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Counter} from "./components/counter/Counter";
+import {Settings} from "./components/settings/Settings";
 
 
 function App() {
 
     return (
         <div className="App">
+            <Settings/>
             <Counter/>
         </div>
     );
