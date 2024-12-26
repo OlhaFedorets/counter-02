fe920802f38eb794879266ae1d12466e04a476d9
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-26 16:39:54 +0300

    change settings

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import style from "../counter/Counter.module.css";
+import {Button} from "../button/Button";
+import s from "../button/Button.module.css";
 
-export const Settings = () => {
+type SettingsTypeProps = {}
+
+export const Settings = ({}: SettingsTypeProps) => {
+    const [maxValue, setMaxValue] = useState('0');
+    const [startValue, setStartValue] = useState('0');
+
+    const classNameForSet = `
+     ${s.button}
+     `
+
+    useEffect(() => {
+        let newMaxValue = localStorage.getItem('maxValue')
+        if (newMaxValue) {
+            setMaxValue(JSON.parse(newMaxValue))
+        }
+ let newStartValue = localStorage.getItem('startValue')
+        if (newStartValue) {
+            setStartValue(JSON.parse(newStartValue))
+        }
+
+    }, [])
+
+    function onChangeMaxValueHandler(event: ChangeEvent<HTMLInputElement>) {
+        setMaxValue(event.currentTarget.value)
+
+    }
+
+    function onChangeStartValueHandler(event: ChangeEvent<HTMLInputElement>) {
+        setStartValue(event.currentTarget.value)
+    }
+
+
+
+
+    function onClickHandler() {
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+    }
+
+
     return (
-        <form>
-            <label htmlFor={'maxValue'}>max value:</label>
-            <input id={'maxValue'} type={''}/>
-            start value:
-            <input/>
-        </form>
+        <div className={style.block}>
+            <div className={style.settings}>
+                <form>
+                    <label htmlFor={'maxValue'}>max value:</label>
+                    <input id={'maxValue'}
+                           type={'number'}
+                           value={maxValue}
+                           onChange={onChangeMaxValueHandler}
+                    />
+
+                    <label htmlFor={'startValue'}>start value:</label>
+                    <input id={'startValue'}
+                           type={'number'}
+                           value={startValue}
+                           onChange={onChangeStartValueHandler}
+                    />
+                </form>
+            </div>
+            <div className={style.frame}>
+                <Button title={'set'}
+                        onClick={onClickHandler}
+                        className={classNameForSet}/>
+            </div>
+        </div>
+
     );
 };
